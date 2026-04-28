2e516a00f0865cdf140b8e615cf44aea72f831be
shriyanshthakur <shriyanshthakur12@gmail.com>
2026-04-29 00:00:33 +0530

    Fixed next config syntax

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
 };
-
-// next.config.js
-module.exports = {
-  allowedDevOrigins: ['172.16.0.2'],
-}
 
 export default nextConfig;
