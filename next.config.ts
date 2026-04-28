77e6abd2815683e2f149a5540b08e176463b65b1
shriyanshthakur <shriyanshthakur12@gmail.com>
2026-04-29 00:11:32 +0530

    Fixed image optimization for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // <-- ADD THIS LINE
+  },
 };
 
 export default nextConfig;
